511696e3fda70035f01d002d97d2ca3dd538aba9
Kamal Pankaj <127144697+Pankajkamal@users.noreply.github.com>
2026-02-18 23:39:49 +0530

    Add project files.

---
+++ Education_WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SofiyaEducation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Program()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+        public IActionResult Donation()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Education_WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Education_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
